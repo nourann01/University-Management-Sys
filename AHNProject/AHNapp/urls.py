23059eadf44cbc2b2a829bd5819b72fbd66bafba
caraxesmsc <mohamedongo21@gmail.com>
2023-12-21 02:51:28 +0200

    login

--- AHNProject/AHNapp/urls.py
+++ AHNProject/AHNapp/urls.py
@@ -8,6 +8,10 @@ urlpatterns = [
     path('courses', views.courses, name='courses'),
     path('drop_course/<int:course_id>/', views.drop_course, name='drop_course'),
     path('add_course/<int:course_id>/', views.add_course, name='add_course'),
+
+    path('signup', views.signup, name='signup'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout'),
     # path('fees', views.fees, name='fees'),
     # path('profile', views.profile, name='profile'),
 ]
